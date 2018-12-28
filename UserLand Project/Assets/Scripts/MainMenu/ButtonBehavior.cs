6bde173604335b0f53e7f78d1664ef3009de33f3
AndreEmiya <34873149+AndreEmiya@users.noreply.github.com>
2018-12-28 04:36:25 +0000

    Main Menu and buttons
    did tons of things sort of

---
+++ UserLand Project/Assets/Scripts/MainMenu/ButtonBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    bool flag = false;
+
+    public void MouseEnter()
+    {
+        flag = true;
+    }
+
+    public void MouseOut()
+    {
+        flag = false;
+    }
+
+    private void Update()
+    {
+        if (flag)
+        {
+            this.GetComponent<Image>().color = Color.white;
+            this.GetComponentInChildren<TextMeshProUGUI>().color = Color.black;
+        }
+        else
+        {
+            this.GetComponent<Image>().color = Color.black;
+            this.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+        }
+    }
+}
